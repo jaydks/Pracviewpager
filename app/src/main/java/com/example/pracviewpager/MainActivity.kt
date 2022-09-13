3dbc213cae1bad170deae662018a2f53d28d0c2d
jaydks <jjeee1313@gmail.com>
2022-09-14 00:03:33 +0900

    뷰페이저 여백 설정 -> 다른 아이템 옆에 조금 보이게

--- app/src/main/java/com/example/pracviewpager/MainActivity.kt
+++ app/src/main/java/com/example/pracviewpager/MainActivity.kt
@@ -17,12 +17,24 @@ class MainActivity : AppCompatActivity() {
 
         val advList = arrayListOf(
             Advs(R.drawable.cat, "고양이"),
-            Advs(R.drawable.dog, "강아지")
+            Advs(R.drawable.dog, "강아지"),
+            Advs(R.drawable.tiger, "호랑이")
         )
 
-        //vp
+        /** view pager */
+        // 여백, 너비에 대한 정의
+        val pageMarginPx = resources.getDimensionPixelOffset(R.dimen.pageMargin) // 여백
+        val pagerWidth = resources.getDimensionPixelOffset(R.dimen.pageMargin) // 페이저 너비
+        val screenWidth = resources.displayMetrics.widthPixels // 스마트폰의 너비 가져옴
+        val offsetPx = screenWidth - pagerWidth - pageMarginPx
 
-        //binding.vpAdv.layoutManager = LinearLayoutManager(this, LinearLayoutManager.HORIZONTAL, false)
+        //setPageTransformer이용해 page의 X축 값에 변화 줌
+        binding.vpAdv.setPageTransformer { page, position ->
+            page.translationX = position * -offsetPx
+
+        }
+
+        binding.vpAdv.offscreenPageLimit = 1 // 몇 개의 페이지를 미리 로드 해둘지
         binding.vpAdv.orientation = ViewPager2.ORIENTATION_HORIZONTAL // 방향
         binding.vpAdv.adapter = AdvViewPagerAdapter(advList)  // 어댑터 생성
 
